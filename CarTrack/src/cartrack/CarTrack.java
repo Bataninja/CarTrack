c21c63658887c7fa76eb4506f9ac8cb32368151a
Frank C <frank@gmail.com>
2025-05-09 20:14:26 -0500

    Se crea el proyecto base
    Java

---
+++ CarTrack/src/cartrack/CarTrack.java
@@ -0,0 +1,14 @@
+package cartrack;
+/**
+ * Clase Main 
+ */
+public class CarTrack {
+
+    /**
+     * @parametro args para el main
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
