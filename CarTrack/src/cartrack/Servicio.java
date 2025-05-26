bdd6d3680043effd6470a7e19a15dbff72d13e69
Frank <frankcamilgordillopea@gmail.com>
2025-05-25 22:29:30 -0500

    Se agrega Enum Tipo servicio
    Igualmente se añade al constructor de la clase Servicio

--- CarTrack/src/cartrack/Servicio.java
+++ CarTrack/src/cartrack/Servicio.java
@@ -1,13 +1,91 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package cartrack;
 
-/**
- *
- * @author Frank
- */
 public class Servicio {
+    
+    /**
+    *  Clase para el objeto servicio con métodos básicos
+    * 
+    *  placa = "" es para no asignar la placa al crearse el objeto, 
+    *  al añadirse a la lista de servicios del vehiculo 
+    *  se actualiza automaticamente en la clase Servicio.
+    */
+
+    private final int idServicio;
+    private final int idTrabajador;
+    private String placa;
+    private String fechaInicio;
+    private String fechaVencimiento;
+    private String descripcion;
+    private TipoServicio tipoServicio;
+    private double valor;
+
+    public Servicio(int idServicio, int idTrabajador, String fechaInicio, String fechaVencimiento, String descripcion, TipoServicio tipoServicio, double valor) {
+        this.idServicio = idServicio;
+        this.idTrabajador = idTrabajador;
+        this.placa = "";
+        this.fechaInicio = fechaInicio;
+        this.fechaVencimiento = fechaVencimiento;
+        this.descripcion = descripcion;
+        this.tipoServicio = tipoServicio;
+        this.valor = valor;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public int getIdServicio() {
+        return idServicio;
+    }
+
+    public int getIdTrabajador() {
+        return idTrabajador;
+    }
+
+    public String getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public TipoServicio getTipoServicio() {
+        return tipoServicio;
+    }
+
+    public void setTipoServicio(TipoServicio tipoServicio) {
+        this.tipoServicio = tipoServicio;
+    }
+    
+    
+    public void setFechaInicio(String fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public String getFechaVencimiento() {
+        return fechaVencimiento;
+    }
+
+    public void setFechaVencimiento(String fechaVencimiento) {
+        this.fechaVencimiento = fechaVencimiento;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+    
     
 }
---
+++ CarTrack/src/cartrack/TipoServicio.java
@@ -0,0 +1,13 @@
+package cartrack;
+
+/**
+ * Enumeradod para tipos de servicio
+ */
+
+public enum TipoServicio {
+    MANTENIMIENTO,
+    REPARACION,
+    LAVADO,
+    REVISION,
+    OTRO
+}
